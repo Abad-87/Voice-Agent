dd8a30cec414bab5f5dbb7d820777451ff560dfe
Abad Mustafa <abadmustafa@gmail.com>
2025-12-20 22:23:42 +0530

    message

--- tts.py
+++ tts.py
@@ -1,10 +1,8 @@
-from piper import PiperVoice
-
-voice = PiperVoice.load("en_US-lessac-medium.onnx")
-
 def text_to_speech(text: str):
     try:
+        from piper import PiperVoice
         import sounddevice as sd
+        voice = PiperVoice.load("en_US-lessac-medium.onnx")
         audio, sample_rate = voice.synthesize(text)
 
         sd.play(audio, sample_rate)
